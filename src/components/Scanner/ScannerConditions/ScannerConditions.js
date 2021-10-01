e6e787aa0f4dd0daefeaf835ae41f3da1cd0edba
suhas-malhotra <xyzcyz00@gmail.com>
2021-10-01 20:15:31 +0530

    double click feature

--- src/components/Scanner/ScannerConditions/ScannerConditions.js
+++ src/components/Scanner/ScannerConditions/ScannerConditions.js
@@ -1,135 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import './ScannerConditions.scss'
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import IndicatorModal from '../IndicatorModal/IndicatorModal';
-
+import React, { useState, useEffect } from "react";
+import "./ScannerConditions.scss";
+import ScannerDraggableComponent from "../ScannerDraggableComponent/ScannerDraggableComponent";
+import IndicatorModal from "../IndicatorModal/IndicatorModal";
+let arr = [];
 const ScannerConditions = () => {
+  const [scannerConditionOptions, setScannerConditionOptions] = useState(false);
+  const [indicatorModalOpen, setIndicatorModalOpen] = useState(false);
+  const [indicatorModalInput, setIndicatorModalInput] = useState({});
+  const [currSelectedIndicator, setCurrSelectedIndicator] = useState([]);
 
-    const [scannerConditionOptions, setScannerConditionOptions] = useState(false);
-    const [indicatorModalOpen, setIndicatorModalOpen] = useState(false);
-    const [indicatorModalInput, setIndicatorModalInput] = useState({});
-    // const [currSelectedIndicator, setCurrSelectedIndicaotr] = useState(null);
+  const closeIndicatorModal = (indicatorSetting) => {
+    indicatorModalInput.element.data = indicatorSetting;
+    setIndicatorModalOpen(false);
+  };
 
-    const closeIndicatorModal = (indicatorSetting) => {
-
-        indicatorModalInput.element.data = indicatorSetting;
-        setIndicatorModalOpen(false);
-    }
-
-    const openIndicatorModal = draggableElement => {
-
-        if(
-            draggableElement.id === '>' 
-            || draggableElement.id === '<' 
-            || draggableElement.id === '+' 
-            || draggableElement.id === '-' 
-            || draggableElement.id === '*'
-        )
-            return;
-
-        setIndicatorModalInput({
-            indicatorName: draggableElement.id,
-            element: draggableElement,
-            settings: [
-                {name: 'Length', value: 14},
-                {name: 'Source', options: ['Open', 'High', 'Low', 'Close'], value: 'Open'}
-            ]
-        });
-        
-        setIndicatorModalOpen(true);
-    }
-
-
-    const deleteOption = async () => {
-
-        const removeAllChild = () => {
-            let scannerDiv = document
-                                .getElementById('scanner-condition-indicators');
-            while(scannerDiv.firstChild)
-                scannerDiv.removeChild(scannerDiv.firstChild);
-        }
-        
-        await removeAllChild();
-        
-        setScannerConditionOptions(false);
-    }
-
-    const scannerOptionDivChecker = () => {
-
-        let scannerConditionOptionsDiv = document
-                                            .getElementById('scanner-condition-indicators');
-        
-        if(scannerConditionOptionsDiv) {
-
-            // console.log(scannerConditionOptionsDiv.childNodes)
-            setScannerConditionOptions(
-                scannerConditionOptionsDiv
-                    .childNodes
-                    .length > 1 ? true : false
-            );
-            
-        }
-
-    }
-
-    const dropElement = e => {
-
-        const id = e.dataTransfer.getData('text');
-        let draggableElement = document.getElementById(id);
-        const dropzone = document.getElementById('scanner-condition-indicators');
-      
-        if(draggableElement) {
-            draggableElement = draggableElement.cloneNode(true);
-            dropzone.appendChild(draggableElement);
-            scannerOptionDivChecker();
-            // console.log(draggableElement, dropzone)
-            // draggableElement.style.display = 'flex';
-            // draggableElement.style.width = '4rem';
-
-            draggableElement.style.textAlign = 'center';
-            draggableElement.style.margin = '1rem'
-            draggableElement.innerText = id.toUpperCase();
-
-            openIndicatorModal(draggableElement);
-
-            // draggableElement.setAttribute('onclick', `this.parentNode.remove();`);
-        }
-
-        e.dataTransfer.clearData();
-    }
-
-    // useEffect(scannerOptionDivChecker, []);
-
-    return (
-        <div className="scanner-conditions">
-            
-            {/* <DeleteOutlinedIcon className="delete-icon" onClick={deleteOption} /> */}
-            {indicatorModalOpen && 
-                <IndicatorModal 
-                    indicatorModalInput = { indicatorModalInput } 
-                    closeIndicatorModal = { closeIndicatorModal }
-                />
-            }
-
-            <div 
-                id="scanner-condition-indicators" 
-                onDragOver={e => e.preventDefault()} 
-                onDrop={dropElement}
-            >
-                
-                { !scannerConditionOptions && (
-                    <div className="scanner-indicator-drag-request">
-                        Drag Something here !!
-                    </div>
-                )}
-                
-            </div>
-
-        </div>
+  const openIndicatorModal = (draggableElement) => {
+    if (
+      draggableElement.id === ">" ||
+      draggableElement.id === "<" ||
+      draggableElement.id === "+" ||
+      draggableElement.id === "-" ||
+      draggableElement.id === "*"
     )
-}
+      return;
+
+    setIndicatorModalInput({
+      indicatorName: draggableElement.id,
+      element: draggableElement,
+      settings: [
+        { name: "Length", value: 14 },
+        {
+          name: "Source",
+          options: ["Open", "High", "Low", "Close"],
+          value: "Open",
+        },
+      ],
+    });
+
+    setIndicatorModalOpen(true);
+  };
+
+  //   const deleteOption = async () => {
+  //     const removeAllChild = () => {
+  //       let scannerDiv = document.getElementById("scanner-condition-indicators");
+  //       while (scannerDiv.firstChild)
+  //         scannerDiv.removeChild(scannerDiv.firstChild);
+  //     };
+
+  //     await removeAllChild();
+
+  //     setScannerConditionOptions(false);
+  //   };
+
+  const dropElement = (e) => {
+    const id = e.dataTransfer.getData("text");
+    let draggableElement = document.getElementById(id);
+    if (draggableElement) {
+      arr.push(draggableElement);
+      setCurrSelectedIndicator(arr);
+      setScannerConditionOptions(arr.length > 0 ? true : false);
+      openIndicatorModal(draggableElement);
+      draggableElement.setAttribute("onclick", `this.parentNode.remove();`);
+    }
+    e.dataTransfer.clearData();
+  };
+
+
+  return (
+    <div className="scanner-conditions">
+      {/* <DeleteOutlinedIcon className="delete-icon" onClick={deleteOption} /> */}
+      {indicatorModalOpen && (
+        <IndicatorModal
+          indicatorModalInput={indicatorModalInput}
+          closeIndicatorModal={closeIndicatorModal}
+        />
+      )}
+
+      <div
+        id="scanner-condition-indicators"
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={dropElement}
+      >
+        {!scannerConditionOptions && (
+          <div className="scanner-indicator-drag-request">
+            Drag Something here !!
+          </div>
+        )}
+        {currSelectedIndicator.map((e) => (
+          <ScannerDraggableComponent id={e.id} draggableElement={e} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ScannerConditions;
-
-
--- src/components/Scanner/ScannerDraggableComponent/ScannerDraggableComponent.js
+++ src/components/Scanner/ScannerDraggableComponent/ScannerDraggableComponent.js
@@ -1,19 +1,61 @@
-import { textAlign } from '@mui/system'
-import React from 'react'
+import React, { useState } from "react";
+import IndicatorModal from "../IndicatorModal/IndicatorModal";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+const ScannerDraggableComponent = ({ id, draggableElement }) => {
+  const [indicatorModalOpen, setIndicatorModalOpen] = useState(false);
+  const [indicatorModalInput, setIndicatorModalInput] = useState({});
+  const closeIndicatorModal = (indicatorSetting) => {
+    indicatorModalInput.element.data = indicatorSetting;
+    setIndicatorModalOpen(false);
+  };
 
-const ScannerDraggableComponent = ({ id }) => {
-    return (
-        <div 
-            className="scanner-indicator-name" 
-            id={id}
-            style={{
-                textAlign: 'center',
-                margin: '1rem'
-            }}
-        >
-            {id.toUpperCase()}
-        </div>
+  const openIndicatorModal = (draggableElement) => {
+    if (
+      draggableElement.id === ">" ||
+      draggableElement.id === "<" ||
+      draggableElement.id === "+" ||
+      draggableElement.id === "-" ||
+      draggableElement.id === "*"
     )
-}
+      return;
 
-export default ScannerDraggableComponent
+    setIndicatorModalInput({
+      indicatorName: draggableElement.id,
+      element: draggableElement,
+      settings: [
+        { name: "Length", value: 14 },
+        {
+          name: "Source",
+          options: ["Open", "High", "Low", "Close"],
+          value: "Open",
+        },
+      ],
+    });
+
+    setIndicatorModalOpen(true);
+  };
+  return (
+    <>
+      <DeleteOutlinedIcon className="delete-icon" />
+      <div
+        className="scanner-indicator-name"
+        id={id}
+        style={{
+          textAlign: "center",
+          margin: "1rem",
+        }}
+        onDoubleClick={() => openIndicatorModal(draggableElement)}
+      >
+        {indicatorModalOpen && (
+          <IndicatorModal
+            indicatorModalInput={indicatorModalInput}
+            closeIndicatorModal={closeIndicatorModal}
+          />
+        )}
+        {id.toUpperCase()}
+      </div>
+    </>
+  );
+};
+
+export default ScannerDraggableComponent;
