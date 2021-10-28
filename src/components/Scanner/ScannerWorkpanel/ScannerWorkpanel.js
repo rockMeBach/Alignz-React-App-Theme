425a48d621708ca831bd67954d39d17708dda1a1
Priyanshu Srivastava <75321175+priyanshu31@users.noreply.github.com>
2021-10-28 17:25:24 +0530

    Api call for mutliple expression done

--- src/components/Scanner/ScannerWorkpanel/ScannerWorkpanel.js
+++ src/components/Scanner/ScannerWorkpanel/ScannerWorkpanel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./ScannerWorkpanel.scss";
 import WorkpanelHeading from "../WorkpanelHeading/WorkpanelHeading";
@@ -11,6 +11,7 @@ const ScannerWorkpanel = ({ scannerResultDisplay }) => {
   const auth = useSelector((state) => state.auth);
   const [data, setData] = useState(null);
   const [scannerInfo, setScannerInfo] = useState(null);
+  const [apiResults, setApiResults] = useState([]);
   const scannerData = (s) => {
     setData(s);
   };
@@ -34,36 +35,41 @@ const ScannerWorkpanel = ({ scannerResultDisplay }) => {
     );
     console.log(res);
   };
-  const fetchScannerResults = async () => {
-    let conditions = document.getElementById(
-      "scanner-condition-indicators"
-    ).childNodes;
+  const fetchScannerResults = async (conditions) => {
+
+    // let conditions = document.getElementById(
+    //   "scanner-condition-indicators"
+    // ).childNodes;
+    
     let flag = true;
     let comparison;
     let LHS = [];
     let RHS = [];
     conditions.forEach((e) => {
-      console.log(e.childNodes[1].id, e.childNodes[1].data);
+      console.log(e)
+      console.log(e.id, e.data);
 
       if (
-        e.childNodes[1].id === "<" ||
-        e.childNodes[1].id === ">" ||
-        e.childNodes[1].id === "cfab" ||
-        e.childNodes[1].id === "cfba" ||
-        e.childNodes[1].id === ">=" ||
-        e.childNodes[1].id === "<="
+        e.id === "<" ||
+        e.id === ">" ||
+        e.id === "cfab" ||
+        e.id === "cfba" ||
+        e.id === ">=" ||
+        e.id === "<="
       ) {
-        comparison = e.childNodes[1].id;
+        comparison = e.id;
         flag = false;
       } else if (flag) {
-        if (e.childNodes[1].data) LHS.push(e.childNodes[1].data);
-        else LHS.push(e.childNodes[1].id);
+        if (e.data) LHS.push(e.data);
+        else LHS.push(e.id);
       } else {
-        if (e.childNodes[1].data) RHS.push(e.childNodes[1].data);
-        else RHS.push(e.childNodes[1].id);
+        if (e.data) RHS.push(e.data);
+        else RHS.push(e.id);
       }
     });
+
     if (!document.getElementById("satisfy").checked) comparison = !comparison;
+    
     let starttime =
       document.getElementById("scanner-start-time").childNodes[1].value;
     let endtime =
@@ -82,16 +88,51 @@ const ScannerWorkpanel = ({ scannerResultDisplay }) => {
     let res = await queryCalculator(query);
     if (res === undefined) res = [];
 
-    console.log(res);
+    console.log(res.data);
 
-    scannerResultDisplay(res.data);
+    let response = apiResults;
+    response.push(res.data);
+    setApiResults(response);
+
+    // return res.data;
+    // scannerResultDisplay(res.data);
   };
+
+  const submitScanner = () => {
+
+    let conditions = document.getElementById(
+      "scanner-condition-indicators"
+    ).childNodes
+
+    let binary_operation = [];
+    conditions.forEach(async e => {
+      console.log(Array.from(e.childNodes));
+
+      if(e.childNodes.length === 1 && (
+          e.childNodes[0].id === 'or' ||
+          e.childNodes[0].id === 'and' ||
+          e.childNodes[0].id === 'substract'
+        ))
+        binary_operation.push(e.childNodes[0].id)
+      else {
+
+        fetchScannerResults(Array.from(e.childNodes))
+      }
+    });
+
+    console.log("Binary", binary_operation)
+  };
+
+  useEffect(() => {
+    
+    console.log("Results", apiResults)
+  }, [apiResults]);
 
   return (
     <div className="col-lg-12 scanner-workpanel-component">
       <WorkpanelHeading scannerData={scannerData} />
       <WorkpanelFilter />
-      <button className="btn submit-btn" onClick={fetchScannerResults}>
+      <button className="btn submit-btn" onClick={submitScanner}>
         Submit
       </button>
       <button className="btn submit-btn" onClick={saveScanner}>
