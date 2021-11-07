3f0d6b8ad6405126e41f4de2f624560d8a5ff3fa
suhas-malhotra <xyzcyz00@gmail.com>
2021-11-07 18:14:09 +0530

    .

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Home.css";
+import "./Home.scss";
 import image1 from "../../assets/images/image1.jpg";
 import image2 from "../../assets/images/image2.png";
 import image3 from "../../assets/images/features-3.png";
@@ -427,6 +427,276 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <section class="testimonial">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 d-none d-lg-block">
+              <ol class="carousel-indicators tabs">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                >
+                  <figure>
+                    <img
+                      src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-01-179x179.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </figure>
+                </li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1">
+                  <figure>
+                    <img
+                      src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-02-306x306.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </figure>
+                </li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2">
+                  <figure>
+                    <img
+                      src="https://livedemo00.template-help.com/wt_62267_v8/prod-20823-one-service/images/testimonials-03-179x179.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </figure>
+                </li>
+              </ol>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center align-items-center">
+              <div
+                id="carouselExampleIndicators"
+                data-interval="false"
+                class="carousel slide"
+                data-ride="carousel"
+              >
+                <h3>WHAT OUR CLIENTS SAY</h3>
+                <h1>TESTIMONIALS</h1>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="quote-wrapper">
+                      <p>
+                        I have tried a lot of food delivery services but Plate
+                        is something out of this world! Their food is really
+                        healthy and it tastes great, which is why I recommend
+                        this company to all my friends!
+                      </p>
+                      <h3>peter lee</h3>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="quote-wrapper">
+                      <p>
+                        I have tried a lot of food delivery services but Plate
+                        is something out of this world! Their food is really
+                        healthy and it tastes great, which is why I recommend
+                        this company to all my friends!
+                      </p>
+                      <h3>peter lee</h3>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="quote-wrapper">
+                      <p>
+                        I have tried a lot of food delivery services but Plate
+                        is something out of this world! Their food is really
+                        healthy and it tastes great, which is why I recommend
+                        this company to all my friends!
+                      </p>
+                      <h3>peter lee</h3>
+                    </div>
+                  </div>
+                </div>
+                <ol class="carousel-indicators indicators">
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    class="active"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div class="faq_area section_padding_130" id="faq">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12 col-sm-8 col-lg-6">
+              <div
+                class="section_heading text-center wow fadeInUp"
+                data-wow-delay="0.2s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: "0.2s",
+                  animationName: "fadeInUp",
+                }}
+              >
+                <h3>
+                  <span>Frequently </span> Asked Questions
+                </h3>
+                <p>
+                  Appland is completely creative, lightweight, clean &amp; super
+                  responsive app landing page.
+                </p>
+                <div class="line"></div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-lg-8">
+              <div class="accordion faq-accordian" id="faqAccordion">
+                <div
+                  class="card border-0 wow fadeInUp"
+                  data-wow-delay="0.2s"
+                  style={{
+                    visibility: "visible",
+                    animationDelay: "0.2s",
+                    animationName: "fadeInUp",
+                  }}
+                >
+                  <div class="card-header" id="headingOne">
+                    <h6
+                      class="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      How can I install this app?
+                      <span class="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    class="collapse"
+                    id="collapseOne"
+                    aria-labelledby="headingOne"
+                    data-parent="#faqAccordion"
+                  >
+                    <div class="card-body">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Architecto quidem facere deserunt sint animi sapiente
+                        vitae suscipit.
+                      </p>
+                      <p>
+                        Appland is completely creative, lightweight, clean &amp;
+                        super responsive app landing page.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="card border-0 wow fadeInUp"
+                  data-wow-delay="0.3s"
+                  style={{
+                    visibility: "visible",
+                    animationDelay: "0.2s",
+                    animationName: "fadeInUp",
+                  }}
+                >
+                  <div class="card-header" id="headingTwo">
+                    <h6
+                      class="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="true"
+                      aria-controls="collapseTwo"
+                    >
+                      The apps isn't installing?
+                      <span class="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    class="collapse"
+                    id="collapseTwo"
+                    aria-labelledby="headingTwo"
+                    data-parent="#faqAccordion"
+                  >
+                    <div class="card-body">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Architecto quidem facere deserunt sint animi sapiente
+                        vitae suscipit.
+                      </p>
+                      <p>
+                        Appland is completely creative, lightweight, clean &amp;
+                        super responsive app landing page.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="card border-0 wow fadeInUp"
+                  data-wow-delay="0.4s"
+                  style={{
+                    visibility: "visible",
+                    animationDelay: "0.2s",
+                    animationName: "fadeInUp",
+                  }}
+                >
+                  <div class="card-header" id="headingThree">
+                    <h6
+                      class="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseThree"
+                      aria-expanded="true"
+                      aria-controls="collapseThree"
+                    >
+                      Contact form isn't working?
+                      <span class="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    class="collapse"
+                    id="collapseThree"
+                    aria-labelledby="headingThree"
+                    data-parent="#faqAccordion"
+                  >
+                    <div class="card-body">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Architecto quidem facere deserunt sint animi sapiente
+                        vitae suscipit.
+                      </p>
+                      <p>
+                        Appland is completely creative, lightweight, clean &amp;
+                        super responsive app landing page.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                class="support-button text-center d-flex align-items-center justify-content-center mt-4 wow fadeInUp"
+                data-wow-delay="0.5s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: "0.2s",
+                  animationName: "fadeInUp",
+                }}
+              >
+                <i class="lni-emoji-sad"></i>
+                <p class="mb-0 px-2">Can't find your answers?</p>
+                <a href="#"> Contact us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div class="footer-newsletter">
         <div class="container">
