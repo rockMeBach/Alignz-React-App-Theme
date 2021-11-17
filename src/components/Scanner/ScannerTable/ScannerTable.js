ac1c42683c9f0f611c864ca008038376eb4473fd
Priyanshu Srivastava <75321175+priyanshu31@users.noreply.github.com>
2021-11-17 15:28:34 +0530

    changes done

--- src/components/Scanner/ScannerTable/ScannerTable.js
+++ src/components/Scanner/ScannerTable/ScannerTable.js
@@ -33,17 +33,51 @@ const ScannerTable = ({key, value, index}) => {
                     </tr>
                 ))
                 :
-                <tr>
-                    <th scope="row">{index + 1}</th>
-                    <td>{value[0].ticker}</td>
-                    <td><strong>NSE</strong></td>
-                    <td>{value[0].close}</td>
-                    <td>{value[0].volume}</td>
-                    <td>{value[0].date}</td>
-                    <td>{value[0].time}</td>
-                    <td>{value[0].lvalue}</td>
-                    <td>{value[0].rvalue}</td>
-                </tr>
+                value.map((e, i) => {
+                    
+                    let previousRow = i ? value[i - 1] : undefined;
+
+                    console.log("if", previousRow)
+                    if(previousRow && previousRow.date === e.date) {
+
+                        let pt = previousRow.time.split(':');
+                        let ct  = e.time.split(':');
+
+
+                        if(
+                            (
+                                Number(ct[0]) === Number(pt[0])
+                                &&
+                                Number(ct[1]) - Number(pt[1]) === 1
+                            )
+                            ||
+                            (
+                                Number(ct[0]) - Number(pt[0]) === 1
+                                && 
+                                Number(ct[1]) === 0
+                                && 
+                                Number(pt[1]) === 59
+                            )
+                        ) {
+                            return <></>;
+                        }
+
+                    }
+                
+                    return (
+                        <tr>
+                            <th scope="row">{i + 1}</th>
+                            <td>{e.ticker}</td>
+                            <td><strong>NSE</strong></td>
+                            <td>{e.close}</td>
+                            <td>{e.volume}</td>
+                            <td>{e.date}</td>
+                            <td>{e.time}</td>
+                            <td>{e.lvalue}</td>
+                            <td>{e.rvalue}</td>
+                        </tr>
+                    )
+                })
             
             }
 
