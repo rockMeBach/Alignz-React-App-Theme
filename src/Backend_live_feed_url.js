2548d8edd8c8ef014e81d19a357900be75c68b33
Unfluke <admin@unfluke.in>
2022-01-05 00:05:54 +0530

    orders final update

--- src/Backend_live_feed_url.js
+++ src/Backend_live_feed_url.js
@@ -1,3 +1,3 @@
-const BACKEND_URL_LIVE_TRADE = "ec2-13-232-136-66.ap-south-1.compute.amazonaws.com"
-// const BACKEND_URL_LIVE_TRADE = "localhost:80"
+// const BACKEND_URL_LIVE_TRADE = "ec2-13-232-136-66.ap-south-1.compute.amazonaws.com"
+const BACKEND_URL_LIVE_TRADE = "localhost:80"
 export default BACKEND_URL_LIVE_TRADE
